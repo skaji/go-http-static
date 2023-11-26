c845e5109057b7073af712d7e9fa744d408a7e45
Shoichi Kaji <skaji@cpan.org>
2023-11-26 20:13:17 +0900

    version

--- main.go
+++ main.go
@@ -9,11 +9,20 @@ import (
 	apachelog "github.com/lestrrat-go/apache-logformat/v2"
 )
 
+var version = "dev"
+
 func main() {
 	port := flag.Int("port", 5001, "port number")
 	sslCrt := flag.String("ssl-crt", "", "ssl crt file")
 	sslKey := flag.String("ssl-key", "", "ssl key file")
+	showVersion := flag.Bool("v", false, "show version and exit")
 	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	if (*sslCrt == "") != (*sslKey == "") {
 		fmt.Fprintln(os.Stderr, "must specify both -sslCrt and -sslKey")
 		os.Exit(1)
